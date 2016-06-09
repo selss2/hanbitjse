1055ba11d9db6bac19a2c6a26f880ffd6e5e9431
hb2016 <hb2016@hb2016-PC>
2016-06-09 16:43:01 +0900

    6.9 1642

---
+++ hello/src/branch/MonthEnd.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package branch;
+
+import java.util.Scanner;
+
+/**
+ * @date :2016. 6. 9.
+ * @author :오승준
+ * @file :MonthEnd.java
+ * @story :
+ */
+public class MonthEnd {
+	/**
+	 * 월을 입력하면 말일이 몇일이 알려주는 프로그램 단, 2월은 29일로 한정함. 1~12를 벗어난 숫자를 입력하면 잘못된 입력값입니다
+	 * 라고 뜬다.
+	 */
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int day = 0, month = 0;
+		System.out.println("월을 입력해주세요");
+		month = scanner.nextInt();
+		switch (month) {
+		case 1:
+		case 3:
+		case 5:
+		case 7:
+		case 8:
+		case 10:
+		case 12:
+			day = 31;
+			break;
+		case 4:
+		case 6:
+		case 9:
+		case 11:
+			day = 30;
+			break;
+		case 2:
+			day = 28;
+			break;
+		default:
+			System.out.println("잘못된 입력값입니다.");
+			return;		}
+		System.out.println("잘못된 입력값입니다");
+		System.out.println("" + month + "월의 말일은 " + day + "일 입니다. ");	}}
