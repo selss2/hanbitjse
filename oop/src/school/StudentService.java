3813ffbfdce91a2eb470654c3a5136dd7bab6494
hb2016 <hb2016@hb2016-PC>
2016-06-17 17:14:09 +0900

    6 17 금 17:14

---
+++ oop/src/school/StudentService.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package school;
+
+/**
+ * @date   :2016. 6. 17.
+ * @author :오승준
+ * @file   :StudentService.java
+ * @story  : 
+ */
+public interface StudentService {
+public void regist();
+public void find();
+public void update();
+public void delete();
+}
